371cf8a6be1293b1b9a12a765692d7d74304092c
sunnyrk <radadiyasunny970@gmail.com>
2025-10-15 10:14:59 +0530

    Refactor remove console

--- test/mocks/MockDeFiPool.sol
+++ test/mocks/MockDeFiPool.sol
@@ -23,19 +23,11 @@ contract MockDeFiPool {
      * @dev Records msg.sender to verify it's the smart account
      */
     function swap(uint256 amountIn, uint256 minAmountOut) external returns (uint256 amountOut) {
-        console.log("\n*** MockDeFiPool.swap() called ***");
-        console.log("msg.sender (should be smart account):", msg.sender);
-        console.log("amountIn:", amountIn);
-        console.log("minAmountOut:", minAmountOut);
-        
         lastCaller = msg.sender;
         callCount++;
         
         // Mock swap logic - just return 90% of input as output
         amountOut = (amountIn * 90) / 100;
-        
-        console.log("amountOut:", amountOut);
-        console.log("*** MockDeFiPool.swap() completed ***\n");
         
         emit Swap(msg.sender, amountIn, amountOut);
         return amountOut;
@@ -45,15 +37,8 @@ contract MockDeFiPool {
      * @notice Mock deposit function
      */
     function deposit(uint256 amount) external {
-        console.log("\n*** MockDeFiPool.deposit() called ***");
-        console.log("msg.sender (should be smart account):", msg.sender);
-        console.log("amount:", amount);
-        
         lastCaller = msg.sender;
         callCount++;
-        
-        console.log("*** MockDeFiPool.deposit() completed ***\n");
-        
         emit Deposit(msg.sender, amount);
     }
     
@@ -61,15 +46,8 @@ contract MockDeFiPool {
      * @notice Mock withdraw function
      */
     function withdraw(uint256 amount) external returns (bool) {
-        console.log("\n*** MockDeFiPool.withdraw() called ***");
-        console.log("msg.sender (should be smart account):", msg.sender);
-        console.log("amount:", amount);
-        
         lastCaller = msg.sender;
         callCount++;
-        
-        console.log("*** MockDeFiPool.withdraw() completed ***\n");
-        
         emit Withdraw(msg.sender, amount);
         return true;
     }
@@ -78,8 +56,6 @@ contract MockDeFiPool {
      * @notice Function that reverts to test error handling
      */
     function failingFunction() external pure {
-        console.log("\n*** MockDeFiPool.failingFunction() called ***");
-        console.log("This will revert!");
         revert("Intentional failure");
     }
     
